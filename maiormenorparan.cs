3e4139244e442f4b8cb95ea318f149a39fed44da
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-05 15:00:27 -0300

    Add files via upload

---
+++ maiormenorparan.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace maiormenorparan
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int numero = 0;
+            var maior = 0;
+            var menor = 10000;
+
+            do
+            {
+                Console.WriteLine("Informe um numero (ou 0 para sair): ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    Console.WriteLine($"O maior valor é {maior}");
+                    Console.WriteLine($"O menor valor é {menor}");
+                }
+                else if (numero > maior)
+                {
+                    maior = numero;
+                }
+                else if (numero < menor)
+                {
+                    menor = numero;
+                }
+            } while (numero != 0);
+        }
+    }
+}     
