6be54990776e0f0c92597d2442d9440952b82b07
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-05 15:17:37 -0300

    Add files via upload

---
+++ MediaAritmetica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro valor: ");
+            double valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor: ");
+            double valor2 = int.Parse(Console.ReadLine());
+
+            double media = (valor1 + valor2) / 2;
+
+            Console.WriteLine($"A média dos valores é: {media}");
+        }
+    }
+}        
