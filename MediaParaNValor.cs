e296fa5fd792c9591a2f7099eb8694a644c2dd5d
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-05 15:30:23 -0300

    Add files via upload

---
+++ MediaParaNValor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MediaParaNValor;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        {
+            var cont = 0;
+            var num = 0;
+            var total = 0;
+            do
+            {
+                Console.WriteLine("Insira o valor (0 para sair): ");
+                num = int.Parse(Console.ReadLine());
+            
+                if (num == 0)
+                {
+                    Console.WriteLine($"A media de todos os valores é {total/cont}");
+                }
+            
+                total += num;
+                cont++;
+
+            } while (num != 0);
+        }
+    }
+}
+    
