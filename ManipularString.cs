ebddc776da19be192ecf048c99ce747b041b161c
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-06 16:52:42 -0300

    Add files via upload

---
+++ ManipularString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ManipularString
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome completo?:\n ");
+            string nome = "Anabelle Rodrigues";
+
+
+            Console.WriteLine(nome.Trim());
+            Console.WriteLine(nome.Replace(" ", ""));
+            Console.WriteLine(nome.ToLower());
+            Console.WriteLine(nome.ToUpper());
+            Console.WriteLine(nome.Insert(7, "Ana"));
+            Console.WriteLine(nome.Remove(9, 9));
+            Console.WriteLine(nome.Contains("Rodrigues"));
+            Console.WriteLine(nome.Length);
+           
+        }
+    }
+}    
