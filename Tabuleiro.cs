e11b091d2ae4e7350296258c7ec2ecab00d5d869
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-11 15:04:21 -0300

    Add files via upload

---
+++ Tabuleiro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número de linhas do tabuleiro: ");
+            int linhas = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Informe o número de colunas do tabuleiro: ");
+            int colunas = int.Parse(Console.ReadLine());
+            
+            Console.Write("#");
+            for(int i = 0; i < linhas; i++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("#");
+
+            for(int i = 0; i < linhas; i++)
+            {
+                Console.Write("#");
+                for(int j = 0; j < colunas; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("#");
+            }
+            Console.Write("#");
+            for (int i = 0; i < linhas; i++)
+            {
+                Console.Write("#");
+            }
+            Console.Write("#");
+        }
+    }
+}    
