3a6ec8b21bb3be67f8fe851ba363ad7ad1e42fb1
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-13 16:08:36 -0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            int b = 1;
+            int c = 0;
+            int cont = 0;
+            Console.WriteLine("Quantos números do fibonacci você quer imprimir?: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nFibonacci:");
+            do
+            {
+                a = b + c;
+                b = c;
+                c = a;
+                
+                Console.WriteLine(a);
+                cont++;
+            } while (cont < num);
+        }
+    }    
+}
