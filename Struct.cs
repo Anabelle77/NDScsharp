447f3276e5e31a8b15f8bff69360ee51330c8d61
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-14 14:52:11 -0300

    Add files via upload

---
+++ Struct.cs
@@ -0,0 +1,24 @@
+﻿namespace Struct;
+public class Struct
+{
+    public static void Main(string[] args)
+    {
+        var usuario = new User("Anabelle", "Rodrigues", 19, 1.63);
+    }
+    struct User
+    {
+        public User(string name, string lastname, int idade, double altura)
+        {
+            Name = name;
+            LastName = lastname;
+            Idade = idade;
+            Altura = altura;
+        }
+        
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+        
+    }
+}
