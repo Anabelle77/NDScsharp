cc127f4bbcc2f44297d61aaf8cc37b55058421f6
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-15 16:23:39 -0300

    Update Struct.cs

--- Struct.cs
+++ Struct.cs
@@ -1,11 +1,12 @@
-﻿namespace Struct;
+namespace Struct;
 public class Struct
 {
     public static void Main(string[] args)
     {
         var usuario = new User("Anabelle", "Rodrigues", 19, 1.63);
     }
-    struct User
+
+    public struct User
     {
         public User(string name, string lastname, int idade, double altura)
         {
@@ -13,12 +14,14 @@ public class Struct
             LastName = lastname;
             Idade = idade;
             Altura = altura;
+            
+            Console.WriteLine("Name: "+Name+", Last Name: "+LastName+", Idade: "+Idade+", Altura: "+Altura);
         }
-        
+
         public string Name { get; set; }
         public string LastName { get; set; }
         public int Idade { get; set; }
         public double Altura { get; set; }
-        
+
     }
 }
