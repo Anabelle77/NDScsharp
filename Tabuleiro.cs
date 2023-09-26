212ee63cc1222be286883d2996cfdd4ba6a08053
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2023-09-26 16:07:50 -0300

    Update Tabuleiro.cs

--- Tabuleiro.cs
+++ Tabuleiro.cs
@@ -1,39 +1,38 @@
 ﻿using System;
-
 namespace Tabuleiro
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Informe o número de linhas do tabuleiro: ");
+            Console.WriteLine("Quantas linhas no tabuleiro?: ");
             int linhas = int.Parse(Console.ReadLine());
             
-            Console.WriteLine("Informe o número de colunas do tabuleiro: ");
+            Console.WriteLine("Quantas colunas no tabuleiro?: ");
             int colunas = int.Parse(Console.ReadLine());
             
             Console.Write("#");
-            for(int i = 0; i < linhas; i++)
+            for(int l = 0; l < linhas; l++)
             {
                 Console.Write("#");
             }
             Console.WriteLine("#");
 
-            for(int i = 0; i < linhas; i++)
+            for(int l = 0; l < linhas; l++)
             {
                 Console.Write("#");
-                for(int j = 0; j < colunas; j++)
+                for(int c = 0; c < colunas; c++)
                 {
                     Console.Write(" ");
                 }
                 Console.WriteLine("#");
             }
             Console.Write("#");
-            for (int i = 0; i < linhas; i++)
+            for (int l = 0; l < linhas; l++)
             {
                 Console.Write("#");
             }
             Console.Write("#");
         }
     }
-}    
+}  
