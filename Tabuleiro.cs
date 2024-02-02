c899d2a9d0bab78e83a020e1597b51598bf69198
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2024-02-02 14:11:13 -0300

    Update Tabuleiro.cs

--- Tabuleiro.cs
+++ Tabuleiro.cs
@@ -1,50 +1,48 @@
-namespace aplicativ
-{ 
-    public class Program
+namespace aplicativo
+public class Program
+{
+    public static void Main()
     {
-        public static void Main()
-        {
-            Console.Clear();
-            Console.WriteLine("Qual tamanho das linhas e colunas do tabuleiro?: ");
-            var input = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Qual tamanho das linhas e colunas do tabuleiro?: ");
+        var input = Console.ReadLine();
 
-            if (input != null)
+        if (input != null)
+        {
+            if (int.TryParse(input, out var tamanho))
             {
-                if (int.TryParse(input, out var tamanho))
+                Console.Write("#");
+                for(var l = 0; l < tamanho; l++)
                 {
                     Console.Write("#");
-                    for(var l = 0; l < tamanho; l++)
-                    {
-                        Console.Write("#");
-                    }
+                }
                     
+                Console.WriteLine("#");
+                for(var l = 0; l < tamanho; l++)
+                {
+                    Console.Write("#");
+                    for(var c = 0; c < tamanho; c++)
+                    {
+                        Console.Write(" ");
+                    }
                     Console.WriteLine("#");
-                    for(var l = 0; l < tamanho; l++)
-                    {
-                        Console.Write("#");
-                        for(var c = 0; c < tamanho; c++)
-                        {
-                            Console.Write(" ");
-                        }
-                        Console.WriteLine("#");
-                    }
+                }
                     
-                    Console.Write("#");
-                    for (var l = 0; l < tamanho; l++)
-                    {
-                        Console.Write("#");
-                    }
-                    Console.Write("#");
-                }
-                else
+                Console.Write("#");
+                for (var l = 0; l < tamanho; l++)
                 {
-                    Main();
+                    Console.Write("#");
                 }
+                 Console.Write("#");
             }
             else
             {
                 Main();
             }
+        }
+        else
+        {
+            Main();
         }
     }
 }
