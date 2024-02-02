c226f0ee340d24381f2409d2090862bbedddbff8
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2024-02-02 14:12:20 -0300

    Update ManipularString.cs

--- ManipularString.cs
+++ ManipularString.cs
@@ -1,23 +1,20 @@
-﻿using System;
+﻿namespace ManipularString;
 
-namespace ManipularString
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Qual o seu nome completo?:\n ");
-            string nome = "Anabelle Rodrigues";
+        Console.WriteLine("Qual o seu nome completo?:\n ");
+        string nome = "Anabelle Rodrigues";
 
-            Console.WriteLine(nome.Trim());
-            Console.WriteLine(nome.Replace(" ", ""));
-            Console.WriteLine(nome.ToLower());
-            Console.WriteLine(nome.ToUpper());
-            Console.WriteLine(nome.Insert(7, "Ana"));
-            Console.WriteLine(nome.Remove(9, 9));
-            Console.WriteLine(nome.Contains("Rodrigues"));
-            Console.WriteLine(nome.Replace(" ", "").Length);
+        Console.WriteLine(nome.Trim());
+        Console.WriteLine(nome.Replace(" ", ""));
+        Console.WriteLine(nome.ToLower());
+        Console.WriteLine(nome.ToUpper());
+        Console.WriteLine(nome.Insert(7, "Ana"));
+        Console.WriteLine(nome.Remove(9, 9));
+        Console.WriteLine(nome.Contains("Rodrigues"));
+        Console.WriteLine(nome.Replace(" ", "").Length);
            
-        }
     }
 }    
