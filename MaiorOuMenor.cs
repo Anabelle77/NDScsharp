4c7f67850eece609dc52c4287fd2533415ec0771
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2024-02-09 15:25:30 -0300

    Update MaiorOuMenor.cs

--- MaiorOuMenor.cs
+++ MaiorOuMenor.cs
@@ -1,51 +1,24 @@
-﻿using System;
+namespace maioroumenor;
 
-namespace maioroumenor
+class Program
 {
-    class Program
+    private static void Main()
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Informe o primeiro numero: ");
-            double num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Informe o segundo numero: ");
-            double num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Informe o terceiro numero: ");
-            double num3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o primeiro numero: ");
+        var num1 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Informe o segundo numero: ");
+        var num2 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Informe o terceiro numero: ");
+        var num3 = Convert.ToDouble(Console.ReadLine());
 
-            double maior;
-
-            if (num1 > num2 && num1 > num3)
-            {
-                maior = num1;
-            }
-            else if (num2 > num1 && num2 > num3)
-            {
-                maior = num2;
-            }
-            else
-            {
-                maior = num3;
-            }
-
-            double menor;
-
-            if (num1 < num2 && num1 < num3)
-            {
-                menor = num1;
-            }
-            else if (num2 < num1 && num2 < num3)
-            {
-                menor = num2;
-            }
-            else
-            {
-                menor = num3;
-            }
-
-            Console.WriteLine(
-                $"O maior valor entre {num1}, {num2} e {num3} é {maior}\nO menor valor entre {num1}, {num2} e {num3} é {menor} ");
-            Console.ReadKey();
-        }
+        double maior = Math.Max(Math.Max(num1, num2), num3);
+        
+        double menor = Math.Min(Math.Min(num1, num2), num3);
+        
+        Console.WriteLine(
+            $"O maior valor entre {num1}, {num2} e {num3} é {maior}\nO menor valor entre {num1}, {num2} e {num3} é {menor} ");
+        Console.ReadKey();
     }
-}    
+}
