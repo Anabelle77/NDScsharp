4497757821cd664f48372b997ce64dd8751a994f
Anabelle <114539967+Anabelle77@users.noreply.github.com>
2024-02-09 15:04:59 -0300

    Update CalculaDora.cs

--- CalculaDora.cs
+++ CalculaDora.cs
@@ -1,45 +1,44 @@
-﻿using System;
-namespace CalculaDora
+﻿namespace CalculaDora;
+
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        Console.WriteLine("BEM-VINDO À Calculadora\nQual operação deseja utilizar?:");
+        Console.WriteLine("1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - DIVISÃO\n0 - sair");
+            
+        int escolha = int.Parse(Console.ReadLine());
+
+        if (escolha == 0)
         {
-            Console.WriteLine("BEM-VINDO À Calculadora\nQual operação deseja utilizar?:");
-            Console.WriteLine("1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - DIVISÃO\n0 - sair");
-            
-            int escolha = int.Parse(Console.ReadLine());
+            Environment.Exit(0);
+        }
 
-            if (escolha == 0)
-            {
-                Environment.Exit(0);
-            }
-
-            Console.WriteLine("Insira um número: ");
-            double num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Insira outro número: ");
-            double num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira um número: ");
+        double num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira outro número: ");
+        double num2 = int.Parse(Console.ReadLine());
             
-            if (escolha == 1)
-            {
-                double op = (num1 + num2);
-                Console.WriteLine($"O resultado é: {op}");
-            }
-            else if (escolha == 2)
-            {
-                double op = (num1 - num2);
-                Console.WriteLine($"O resultado é: {op}");
-            }
-            else if (escolha == 3)
-            {
-                double op = (num1 * num2);
-                Console.WriteLine($"O resultado é: {op}");
-            }
-            else if (escolha == 4)
-            {
-                double op = (num1 / num2);
-                Console.WriteLine($"O resultado é: {op}");
-            }
+        if (escolha == 1)
+        {
+            double op = (num1 + num2);
+            Console.WriteLine($"O resultado é: {op}");
+        }
+        else if (escolha == 2)
+        {
+            double op = (num1 - num2);
+            Console.WriteLine($"O resultado é: {op}");
+        }
+        else if (escolha == 3)
+        {
+            double op = (num1 * num2);
+            Console.WriteLine($"O resultado é: {op}");
+        }
+        else if (escolha == 4)
+        {
+            double op = (num1 / num2);
+            Console.WriteLine($"O resultado é: {op}");
         }
     }
 }
+
